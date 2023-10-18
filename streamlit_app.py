8f15789e7881b8f10a5daa796219820047b9c1b5
Saravanakumar.M <saravanakumar.murugesan@gmail.com>
2023-10-18 15:19:23 +0530

    Update streamlit_app.py
    made changes to header and text style

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit
 ## print("Hello, World - Streamlit");
 streamlit.title('My Parents New Healthy Diner');
 
-streamlit.title('Breakfast Menu');
-streamlit.title('Omega 2 & Blueberry Oatmeal');
-streamlit.title('Kale, Spinach & Rocket Smoothie');
-streamlit.title('Hard-Boiled Fre-Range Egg');
+streamlit.Header('Breakfast Menu');
+streamlit.text('Omega 2 & Blueberry Oatmeal');
+streamlit.text('Kale, Spinach & Rocket Smoothie');
+streamlit.text('Hard-Boiled Fre-Range Egg');
