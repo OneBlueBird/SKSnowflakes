b87cca4e7682fb445b3f1844824e75e6fde44dd7
Saravanakumar.M <saravanakumar.murugesan@gmail.com>
2023-10-18 15:19:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit
 ## print("Hello, World - Streamlit");
 streamlit.title('My Parents New Healthy Diner');
 
-streamlit.Header('Breakfast Menu');
+streamlit.header('Breakfast Menu');
 streamlit.text('Omega 2 & Blueberry Oatmeal');
 streamlit.text('Kale, Spinach & Rocket Smoothie');
 streamlit.text('Hard-Boiled Fre-Range Egg');
