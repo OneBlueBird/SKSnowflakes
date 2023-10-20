0ec383bf4cafbaa07b99ba4c0a6def561d3e25ba
Saravanakumar.M <saravanakumar.murugesan@gmail.com>
2023-10-20 23:25:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,13 +41,23 @@ fruits_selected = streamlit.multiselect('Pick some fruits: ', list(my_fruit_list
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.");
+    else:
+        streamlit.write('The user entered ', fruit_choice)
 
-streamlit.header('Fruityvise Fruit Advice');
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi");
-#streamlit.text(fruityvice_response.json())
+        streamlit.header('Fruityvise Fruit Advice');
+        fruityvice_respose = requests.get("https://fruityvice.com/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json.normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+      
+        # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi");
+        #  streamlit.text(fruityvice_response.json())
+except URLError as e:
+	streamlit.error();
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
